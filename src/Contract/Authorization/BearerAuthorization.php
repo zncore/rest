0d28429acd196718bf3004255265ec0501392f73
ZnDeveloper <zndev@ya.ru>
2022-07-02 09:39:20 +0600

    upd

--- src/Contract/Authorization/BearerAuthorization.php
+++ src/Contract/Authorization/BearerAuthorization.php
@@ -3,14 +3,9 @@
 namespace ZnLib\Rest\Contract\Authorization;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\RequestOptions;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
-use Symfony\Component\Cache\CacheItem;
-use ZnCore\Base\Develop\Helpers\DeprecateHelper;
 use ZnCore\Base\DotEnv\Domain\Exceptions\EnvConfigException;
 use ZnCore\Base\FileSystem\Helpers\FilePathHelper;
-use ZnLib\Components\Http\Enums\HttpHeaderEnum;
-use ZnLib\Components\Http\Enums\HttpMethodEnum;
 
 class BearerAuthorization implements AuthorizationInterface
 {
@@ -53,52 +48,5 @@ class BearerAuthorization implements AuthorizationInterface
     {
         $this->currentAuth = [];
         return $this;
-    }
-
-    public function getAuthToken(): ?string
-    {
-        DeprecateHelper::hardThrow();
-        if (empty($this->currentAuth['login'])) {
-            return null;
-        }
-
-        /** @var CacheItem $cacheItem */
-        $cacheItem = $this->authCache->getItem('token_by_login_' . $this->currentAuth['login']);
-        $authToken = $cacheItem->get();
-
-        if ($authToken) {
-
-        } else {
-            $authToken = $this->authorization();
-            //$this->setAuthToken($authToken);
-        }
-        return $authToken;
-    }
-
-    public function authorization()
-    {
-        DeprecateHelper::hardThrow();
-        $options = [
-            RequestOptions::FORM_PARAMS => [
-                'login' => $this->currentAuth['login'],
-                'password' => $this->currentAuth['password'],
-            ],
-        ];
-        $response = $this->guzzleClient->request(HttpMethodEnum::POST, $this->authUri, $options);
-        //$authToken = RestHelper::getBodyAttribute($response, 'token');
-        dd($this->guzzleClient);
-        $authToken = $response->getHeader(HttpHeaderEnum::AUTHORIZATION)[0];
-        $this->setAuthToken($authToken);
-        return $authToken;
-    }
-
-    protected function setAuthToken(string $authToken)
-    {
-        DeprecateHelper::hardThrow();
-        /** @var CacheItem $cacheItem */
-        $cacheItem = $this->authCache->getItem('token_by_login_' . $this->currentAuth['login']);
-        $cacheItem->set($authToken);
-        $cacheItem->expiresAfter(60);
-        $this->authCache->save($cacheItem);
     }
 }
